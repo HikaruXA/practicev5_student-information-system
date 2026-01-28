967f31b70119c4adc08f4552c1747a5a5c11e324
HikaruXA <quaintuser321@gmail.com>
2026-01-28 12:31:55 +0800

    Implement student resource management with CRUD operations and validation

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StudentStoreRequest;
+use App\Http\Requests\StudentUpdateRequest;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::where('user_id', auth()->user()->id)->paginate(10);
+
+        return view('pages.students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StudentStoreRequest $request)
+    {
+        $validated = $request->validated();
+
+        Student::create($validated);
+
+        return redirect()->route('students.index')->with('success', 'Student added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Student $student)
+    {
+        return view('pages.students.show', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Student $student)
+    {
+        return view('pages.students.edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StudentUpdateRequest $request, Student $student)
+    {
+        $validated = $request->validated();
+
+        $student->update($validated);
+
+        return redirect()->route('students.index')->with('success', 'Student updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+
+        return redirect()->route('students.index')->with('success', 'Student deleted succesfully.');
+    }
+}
