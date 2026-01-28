e1a5292c85047cebebb1523be726892d6dcae332
HikaruXA <quaintuser321@gmail.com>
2026-01-28 23:40:22 +0800

    Add AssessmentController for managing assessments with CRUD operations

---
+++ app/Http/Controllers/AssessmentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Assessment;
+use Illuminate\Http\Request;
+
+class AssessmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $assessments = Assessment::paginate(10);
+
+        return view('pages.assessments.index', compact('assessments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.assessments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validated();
+
+        Assessment::create($validated);
+
+        return redirect()->route('assessments.index')->with('success', 'Assessment created succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Assessment $assessment)
+    {
+        return view('pages.assessments.show', compact('assessment'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Assessment $assessment)
+    {
+        return view('pages.sections.edit', compact('assessment'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Assessment $assessment)
+    {
+        $validated = $request->validated();
+
+        $assessment->update($validated);
+
+        return redirect()->route('assessments.index')->with('success', 'Assessment updated succesfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Assessment $assessment)
+    {
+        $assessment->delete();
+
+        return redirect()->route('assessments.index')->with('success', 'Assessment deleted successfully');
+    }
+}
