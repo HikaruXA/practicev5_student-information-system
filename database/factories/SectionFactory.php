911b43899681543ceb6107137e8654aae71427f4
HikaruXA <quaintuser321@gmail.com>
2026-01-31 19:28:32 +0800

    Add SectionFactory and SectionSeeder for database seeding

---
+++ database/factories/SectionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Section>
+ */
+class SectionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $startYear = $this->faker->numberBetween(2020, 2025);
+        $endYear = $startYear + 1;
+
+        return [
+            'user_id' => User::factory(),
+            'name' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'school_year' => "{$startYear}-{$endYear}",
+        ];
+    }
+}
